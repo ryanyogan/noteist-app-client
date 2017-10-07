9ea956c66fdb812169370961d3fa5247b6ca0372
Ryan Yogan <ryanyogan@gmail.com>
2017-10-07 11:13:23 -0700

    added in basic Cognito federated auth

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export default {
+  cognito: {
+    USER_POOL_ID: 'us-east-2_doANWI3ni',
+    APP_CLIENT_ID: '5k9rh5kstbrqchqptehktvu7ls',
+  },
+};
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 import { Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import {
+  CognitoUserPool,
+  AuthenticationDetails,
+  CognitoUser,
+} from 'amazon-cognito-identity-js';
+
+import config from '../config';
 import './Login.css';
 
 class Login extends Component {
@@ -8,10 +15,37 @@ class Login extends Component {
     password: '',
   };
 
+  login(email, password) {
+    const userPool = new CognitoUserPool({
+      UserPoolId: config.cognito.USER_POOL_ID,
+      ClientId: config.cognito.APP_CLIENT_ID,
+    });
+
+    const user = new CognitoUser({ Username: email, Pool: userPool });
+    const authenticationData = { Username: email, Password: password };
+    const authenticationDetails = new AuthenticationDetails(authenticationData);
+
+    return new Promise((resolve, reject) =>
+      user.authenticateUser(authenticationDetails, {
+        onSuccess: () => resolve(),
+        onFailure: err => reject(err),
+      }),
+    );
+  }
+
   _handleChange = event =>
     this.setState({ [event.target.id]: event.target.value });
 
-  _handleSubmit = event => event.preventDefault();
+  _handleSubmit = async event => {
+    event.preventDefault();
+
+    try {
+      await this.login(this.state.email, this.state.password);
+      alert('Logged In'); // eslint-disable-line
+    } catch (error) {
+      alert(error); // eslint-disable-line
+    }
+  };
 
   _validateForm = () =>
     this.state.email.length > 0 && this.state.password.length > 0;
