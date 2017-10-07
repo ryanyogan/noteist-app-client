30a86a384681083719c31cd932bfb6a45efde701
Ryan Yogan <ryanyogan@gmail.com>
2017-10-07 10:39:42 -0700

    added in a 404 page

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './containers/Home';
+import NotFound from './components/NotFound';
 
 export default () => (
   <Switch>
     <Route path="/" exact component={Home} />
+
+    { /* Catch * routes, not found, etc... */ }
+    <Route component={NotFound} />
   </Switch>
 );
---
+++ src/components/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './NotFound.css';
+
+export default () => (
+  <div className="NotFound">
+    <h3>
+     Sorry, you need GPS or we are both really lost (4 "oh" 4)
+    </h3>
+  </div>
+);
